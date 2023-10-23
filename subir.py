5b2311ae01f2c8637ef83d68ec199c50403313d4
BahironBonilla <bjavierbonilla@unicesar.edu.co>
2023-10-23 15:54:39 -0500

    back listo

--- subir.py
+++ subir.py
@@ -1,17 +1,61 @@
 import boto3
-from botocore.client import Config
+
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
-# Define el nombre del bucket
-BUCKET_NAME = 'bukcketlabsebastian'
+AWS_ACCESS_KEY_ID = 'AKIA247MBUKIV45MHGGS'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_REGION = 'us-east-2'  # Reemplaza con tu región
+S3_BUCKET_NAME = 'bukcketlabsebastian'
 
-# Crea una instancia del cliente de S3
-s3 = boto3.resource('s3')
-data = open('e.jpg', 'rb')
-
-s3.Bucket(BUCKET_NAME).put_object(Key='e.jpg', Body=data)
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+import os
 
 
-print("Done")
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION
+    )
+
+    try:
+        with file.file as f:
+            s3_client.upload_fileobj(f, S3_BUCKET_NAME, file.filename)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+    return JSONResponse(content={"message": "Archivo cargado exitosamente"})
+
+@app.get("/list/")
+async def list_files():
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION
+    )
+
+    try:
+        response = s3_client.list_objects(Bucket=S3_BUCKET_NAME)
+        files = [obj['Key'] for obj in response.get('Contents', [])]
+        return JSONResponse(content={"files": files})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
