75770f6ff3f59ed803482e3d5de9f76c88dc5634
BahironBonilla <bjavierbonilla@unicesar.edu.co>
2023-10-23 15:51:29 -0500

    .

--- main.py
+++ main.py
@@ -1,57 +1,61 @@
-import os
-from flask import Flask, request, jsonify
 import boto3
-from botocore.exceptions import NoCredentialsError
-from flask import Flask
-from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
 
-app = Flask(__name__)
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-# Configura las credenciales de AWS
+
+
 AWS_ACCESS_KEY_ID = 'AKIA247MBUKIV45MHGGS'
 AWS_SECRET_ACCESS_KEY = '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'
+AWS_REGION = 'us-east-2'  # Reemplaza con tu región
 S3_BUCKET_NAME = 'bukcketlabsebastian'
 
-s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+import os
 
-# Ruta para subir un archivo a S3
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            try:
-                data = open(file, 'rb')
-                s3.Bucket(S3_BUCKET_NAME).put_object(Key=file, Body=data)
-                #s3.upload_fileobj(file, S3_BUCKET_NAME, file.filename)
-                return jsonify({"message": "Archivo subido exitosamente"})
-            except NoCredentialsError:
-                return jsonify({"error": "Credenciales de AWS no válidas"})
-        else:
-            return jsonify({"error": "No se proporcionó ningún archivo en la solicitud"})
 
-# Ruta para listar todos los archivos en S3
-@app.route('/list', methods=['GET'])
-def list_files():
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION
+    )
+
     try:
-        objects = s3.list_objects_v2(Bucket=S3_BUCKET_NAME)
-        file_list = [obj['Key'] for obj in objects.get('Contents', [])]
-        return jsonify({"files": file_list})
-    except NoCredentialsError:
-        return jsonify({"error": "Credenciales de AWS no válidas"})
+        with file.file as f:
+            s3_client.upload_fileobj(f, S3_BUCKET_NAME, file.filename)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
-# Ruta para eliminar un archivo de S3
-@app.route('/delete/<file_name>', methods=['DELETE'])
-def delete_file(file_name):
+    return JSONResponse(content={"message": "Archivo cargado exitosamente"})
+
+@app.get("/list/")
+async def list_files():
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION
+    )
+
     try:
-        s3.delete_object(Bucket=S3_BUCKET_NAME, Key=file_name)
-        return jsonify({"message": f"Archivo '{file_name}' eliminado exitosamente"})
-    except NoCredentialsError:
-        return jsonify({"error": "Credenciales de AWS no válidas"})
+        response = s3_client.list_objects(Bucket=S3_BUCKET_NAME)
+        files = [obj['Key'] for obj in response.get('Contents', [])]
+        return JSONResponse(content={"files": files})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
-if __name__ == '__main__':
-    app.run(debug=True)
